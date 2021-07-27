1c569dd92577020556dc451949c718cd292eddb5
Hugo Oliveira <hugo684@gmail.com>
2021-07-27 17:12:07 -0300

    Added messages funcionality between users

--- django_saas/settings.py
+++ django_saas/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'django_saas.apps.accounts.apps.AccountsConfig',
+    'django_saas.apps.user_messages.apps.UserMessagesConfig'
 ]
 
 
